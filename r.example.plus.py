421ae61aaf454fb67d55978d41975db65d0da196
Vaclav Petras <wenzeslaus@gmail.com>
2020-01-28 11:07:21 -0500

    Chnage the parameter names to be more aligned with what is actually used

--- r.example.plus.py
+++ r.example.plus.py
@@ -7,7 +7,7 @@ AUTHOR(S): Vaclav Petras <wenzeslaus gmail com>
 
 PURPOSE:   Add two raster maps (an example of a module in Python)
 
-COPYRIGHT: (C) 2018 by Vaclav Petras and the GRASS Development Team
+COPYRIGHT: (C) 2018-2020 by Vaclav Petras and the GRASS Development Team
 
 This program is free software under the GNU General Public
 License (>=v2). Read the file COPYING that comes with GRASS
@@ -21,14 +21,15 @@ for details.
 #% keyword: sum
 #%end
 #%option G_OPT_R_INPUT
-#% key: araster
-#% description: Name of input raster A in an expression A + B
+#% key: a_input
+#% label: Name of input raster A in an expression A + B
 #%end
 #%option G_OPT_R_INPUT
-#% key: braster
-#% description: Name of input raster B in an expression A + B
+#% key: b_input
+#% label: Name of input raster B in an expression A + B
 #%end
 #%option G_OPT_R_OUTPUT
+#% label: Name of result of an expression A + B
 #%end
 
 
@@ -39,11 +40,11 @@ import grass.script as gs
 
 def main():
     options, flags = gs.parser()
-    araster = options['araster']
-    braster = options['braster']
+    a_input = options['a_input']
+    b_input = options['b_input']
     output = options['output']
 
-    gs.mapcalc('{r} = {a} + {b}'.format(r=output, a=araster, b=braster))
+    gs.mapcalc('{r} = {a} + {b}'.format(r=output, a=a_input, b=b_input))
 
     return 0
 
